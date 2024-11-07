2257451577200478ffb41148d6652fc7a3b6658d
ls06260330 <lzhouyan0501@gmail.com>
2024-11-07 23:41:07 +0800

    2024-11-7

---
+++ demo/v1/add.cpp
@@ -0,0 +1,7 @@
+#include "head.h"
+#include <iostream>
+
+double add(double a, double b)
+{
+    return a + b;
+}
---
+++ demo/v1/sub.cpp
@@ -0,0 +1,7 @@
+#include "head.h"
+#include <iostream>
+
+double sub(double a, double b)
+{
+    return a * b;
+}
